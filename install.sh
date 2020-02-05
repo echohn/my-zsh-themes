a63eafaeb68d33b63ddf7761bdee482e66978e31
echohn <echohn@gmail.com>
2020-02-05 22:18:24 +0800

    change theme link path

--- install.sh
+++ install.sh
@@ -1,3 +1,6 @@
 #!/bin/zsh
 
-ln -s $(cd "$(dirname "$0")"; pwd) ~/.oh-my-zsh/custom/themes/echohn
+[ ! -d ~/.oh-my-zsh/custom/themes ] && mkdir -p ~/.oh-my-zsh/custom/themes/
+ln -s $(cd "$(dirname "$0")"; pwd)/echohn_agnoster.zsh-theme ~/.oh-my-zsh/custom/themes/
+
+
